cf22429d27f43a26b1255ce80277ad698ab5ad23
vyushin_aa <ledford1403@gmail.com>
2023-09-29 10:30:36 +0300

    Добавил описание для базовой сущности, сущности с именем и сущность RoleType

---
+++ src/main/java/com/example/searchteam/domain/role/RoleType.java
@@ -0,0 +1,65 @@
+package com.example.searchteam.domain.role;
+
+import com.example.searchteam.domain.EntityWithName;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.Accessors;
+
+import java.io.Serial;
+import java.time.LocalDateTime;
+
+@Entity(name = "ROLE_TYPE")
+@Getter
+@Setter
+@Accessors(chain = true)
+@NoArgsConstructor
+@ToString(onlyExplicitlyIncluded = true, callSuper = true)
+@EqualsAndHashCode(callSuper = true, onlyExplicitlyIncluded = true)
+@AttributeOverride(name = "id", column = @Column(name = "ROLE_TYPE_ID"))
+public class RoleType extends EntityWithName {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public RoleType(Long roleTypeId) {
+        this.id = roleTypeId;
+    }
+
+
+    @Override
+    public RoleType setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    @Override
+    public RoleType setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    @Override
+    public RoleType setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    @Override
+    public RoleType setCreatedDateTime(LocalDateTime createdDateTime) {
+        this.createdDateTime = createdDateTime;
+        return this;
+    }
+
+    @Override
+    public RoleType setModifiedDateTime(LocalDateTime modifiedDateTime) {
+        this.modifiedDateTime = modifiedDateTime;
+        return this;
+    }
+
+}
