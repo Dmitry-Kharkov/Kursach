885cfbd2650d7c9d3aac5fb4d6f81adaad92469c
vyushin_aa <ledford1403@gmail.com>
2023-10-26 21:29:37 +0300

    Init config

---
+++ src/main/java/com/example/searchteam/config/InitConfig.java
@@ -0,0 +1,34 @@
+package com.example.searchteam.config;
+
+import com.example.searchteam.dto.request.user.UserAddRequest;
+import com.example.searchteam.service.domain.user.UserDomainService;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class InitConfig {
+
+    private final UserDomainService userDomainService;
+
+    @Value("${search_team.admin.login}")
+    private String login;
+
+    @Value("${search_team.admin.pwd}")
+    private String password;
+
+
+    @PostConstruct
+    public void initAdmin(){
+        if (userDomainService.getUserByLogin(login).getUserId() == null){
+           var user =  userDomainService.addUser(new UserAddRequest().setLogin(login).setPassword(password));
+           userDomainService.setUserRole(user, List.of(0L));
+        }
+    }
+
+}
