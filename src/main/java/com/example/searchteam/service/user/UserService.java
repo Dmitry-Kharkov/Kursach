057addf4b9647d4680229561f40aee9a4aa38988
vyushin_aa <ledford1403@gmail.com>
2023-10-26 20:50:45 +0300

    clean up

--- src/main/java/com/example/searchteam/service/user/UserService.java
+++ src/main/java/com/example/searchteam/service/user/UserService.java
@@ -1,13 +1,8 @@
 package com.example.searchteam.service.user;
 
-import com.example.searchteam.dto.request.applicant.ApplicantAddRequest;
-import com.example.searchteam.dto.request.team.TeamRequest;
 import com.example.searchteam.dto.request.user.UserAddRequest;
 import com.example.searchteam.dto.request.user.UserRequest;
-import com.example.searchteam.dto.response.applicant.ApplicantResponse;
-import com.example.searchteam.dto.response.team.TeamResponse;
 import com.example.searchteam.dto.response.user.UserResponse;
-import com.example.searchteam.service.domain.team.TeamDomainService;
 import com.example.searchteam.service.domain.user.UserDomainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
