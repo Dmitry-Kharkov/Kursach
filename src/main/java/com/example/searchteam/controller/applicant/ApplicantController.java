e1612bd5bae6857a9620e1d8fc51d19792d15bc0
harkov.dima2005@gmail.com <harkov.dima2005@gmail.com>
2023-11-08 22:34:03 +0300

    Сервисы для Applicant

--- src/main/java/com/example/searchteam/controller/applicant/ApplicantController.java
+++ src/main/java/com/example/searchteam/controller/applicant/ApplicantController.java
@@ -2,6 +2,7 @@ package com.example.searchteam.controller.applicant;
 
 import com.example.searchteam.dto.request.applicant.ApplicantAddRequest;
 import com.example.searchteam.dto.request.applicant.ApplicantRequest;
+import com.example.searchteam.dto.request.role.RoleRequest;
 import com.example.searchteam.dto.response.applicant.ApplicantResponse;
 import com.example.searchteam.service.applicant.ApplicantService;
 import lombok.RequiredArgsConstructor;
@@ -20,6 +21,7 @@ public class ApplicantController {
     public static final String APPLICANT_GET_BY_ID = "/api/v1/applicant/get-by-id";
     public static final String APPLICANT_ADD = "/api/v1/applicant/add";
     public static final String APPLICANT_EDIT = "/api/v1/applicant/edit";
+    public static final String APPLICANT_DELETE = "/api/v1/applicant/delete";
     public static final String APPLICANT_GET_ALL = "/api/v1/applicant/get-all";
 
     private final ApplicantService service;
@@ -54,5 +56,13 @@ public class ApplicantController {
             consumes = APPLICATION_JSON_VALUE,
             produces = APPLICATION_JSON_VALUE)
     public List<ApplicantResponse> getAllApplicants(){ return service.getAllApplicants(); }
+
+    @PostMapping(
+            value = APPLICANT_DELETE,
+            consumes = APPLICATION_JSON_VALUE,
+            produces = APPLICATION_JSON_VALUE)
+    public void deleteApplicant(@RequestBody ApplicantRequest request) {
+        service.deleteApplicant(request);
+    }
 
 }
--- src/main/java/com/example/searchteam/service/applicant/ApplicantService.java
+++ src/main/java/com/example/searchteam/service/applicant/ApplicantService.java
@@ -2,6 +2,7 @@ package com.example.searchteam.service.applicant;
 
 import com.example.searchteam.dto.request.applicant.ApplicantAddRequest;
 import com.example.searchteam.dto.request.applicant.ApplicantRequest;
+import com.example.searchteam.dto.request.role.RoleRequest;
 import com.example.searchteam.dto.response.applicant.ApplicantResponse;
 import com.example.searchteam.service.domain.applicant.ApplicantDomainService;
 import lombok.RequiredArgsConstructor;
@@ -32,5 +33,8 @@ public class ApplicantService {
     public ApplicantResponse editApplicant(ApplicantAddRequest request) {
         Long applicantId = service.editApplicant(request);
         return service.getApplicantById(applicantId);
+    }
+    public void deleteApplicant(ApplicantRequest request) {
+        service.deleteApplicantById(request.getApplicantId());
     }
 }
