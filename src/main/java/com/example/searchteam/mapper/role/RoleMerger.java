7892f126348ace2685890bcc37318e68a6fd9367
harkov.dima2005@gmail.com <harkov.dima2005@gmail.com>
2023-11-08 22:52:27 +0300

    Сервисы для Role

--- src/main/java/com/example/searchteam/mapper/role/RoleMerger.java
+++ src/main/java/com/example/searchteam/mapper/role/RoleMerger.java
@@ -1,6 +1,7 @@
 package com.example.searchteam.mapper.role;
 
 import com.example.searchteam.domain.role.Role;
+import com.example.searchteam.domain.role.RoleType;
 import com.example.searchteam.dto.request.role.RoleAddRequest;
 import com.example.searchteam.mapper.Merger;
 import lombok.RequiredArgsConstructor;
@@ -10,12 +11,11 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class RoleMerger implements Merger<Role, RoleAddRequest> {
 
-    private final RoleTypeResponseMapper roleTypeResponseMapper;
     @Override
     public Role merge(Role target, RoleAddRequest source) {
         return target
                 .setName(source.getName())
-                .setDescription(source.getDescription());
-                //.setRoleType(roleTypeResponseMapper.from(source.getRoleType());
+                .setDescription(source.getDescription())
+                .setRoleType(new RoleType(source.getRoleTypeId()));
     }
 }
