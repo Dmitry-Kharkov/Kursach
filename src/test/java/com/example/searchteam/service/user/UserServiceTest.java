af20f7da68c19e31593459bc562c16bb088e64b5
harkov.dima2005@gmail.com <harkov.dima2005@gmail.com>
2023-12-05 17:43:18 +0300

    Unit-тесты для UserService

--- src/test/java/com/example/searchteam/service/user/UserServiceTest.java
+++ src/test/java/com/example/searchteam/service/user/UserServiceTest.java
@@ -1,6 +1,10 @@
 package com.example.searchteam.service.user;
 
 import com.example.searchteam.dto.request.user.UserAddRequest;
+import com.example.searchteam.dto.request.user.UserEditPasswordRequest;
+import com.example.searchteam.dto.request.user.UserEditRolesRequest;
+import com.example.searchteam.dto.request.user.UserRequest;
+import com.example.searchteam.dto.response.role.RoleResponse;
 import com.example.searchteam.dto.response.user.UserResponse;
 import com.example.searchteam.service.domain.user.UserDomainService;
 import org.junit.jupiter.api.Assertions;
@@ -16,6 +20,7 @@ import javax.print.attribute.standard.MediaSize;
 import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -33,6 +38,9 @@ import static org.mockito.Mockito.when;
     private static final String NAME = "NAME";
     private static final String LOGIN = "LOGIN";
 
+    private static final String PASSWORD = "PASSWORD";
+    private static final List<Long> ROLES=null;
+
     private static final LocalDateTime CREATED = LocalDateTime.now();
     private static final LocalDateTime MODIFIED = LocalDateTime.now().plusMinutes(2);
     @Mock
@@ -41,6 +49,22 @@ import static org.mockito.Mockito.when;
     @InjectMocks
     private UserService service;
 
+
+    @Test
+    void getUserByIdTest() {
+        when(domainService.getUserById(any())).thenReturn(getUserResponse());
+
+        var result = service.getUserById(getUserRequest());
+
+        assertEquals(ID, result.getUserId());
+        assertEquals(NAME,result.getFullName());
+        assertEquals(LOGIN,result.getLogin());
+        assertEquals(CREATED, result.getCreated());
+        assertEquals(MODIFIED, result.getModified());
+
+        verify(domainService).getUserById(any());
+        verifyNoMoreInteractions(domainService);
+    }
 
     @Test
     void addUserExceptionTest(){
@@ -71,6 +95,88 @@ import static org.mockito.Mockito.when;
 
     }
 
+    @Test
+    void editPasswordUserExceptionTest(){
+        var request = getUserEditPasswordRequest("123");
+        assertThrows(ResponseStatusException.class,() -> service.editPasswordUser(request));
+    }
+
+
+    @Test
+    void getAllUsersTest() {
+        when(domainService.getAllUsers()).thenReturn(List.of(getUserResponse(),getUserResponse(),getUserResponse()));
+        var result = service.getAllUsers();
+        for(UserResponse e:result) {
+            assertEquals(ID, e.getUserId());
+            assertEquals(LOGIN, e.getLogin());
+            assertEquals(NAME, e.getFullName());
+            assertEquals(ROLES, e.getRoles());
+            assertEquals(CREATED, e.getCreated());
+            assertEquals(MODIFIED, e.getModified());
+        }
+        verify(domainService).getAllUsers();
+        verifyNoMoreInteractions(domainService);
+    }
+
+    @Test
+    void editUserTest() {
+        when(domainService.editUser(any())).thenReturn(getUserResponse().getUserId());
+        when(domainService.getUserById(any())).thenReturn(getUserResponse());
+
+        var result = service.editUser(getUserAddRequest(any()));
+
+        assertEquals(ID, result.getUserId());
+        assertEquals(NAME,result.getFullName());
+        assertEquals(LOGIN, result.getLogin());
+        assertTrue(result.getRoles().isEmpty());
+        assertEquals(CREATED,result.getCreated());
+        assertEquals(MODIFIED, result.getModified());
+
+        verify(domainService).editUser(any());
+        verify(domainService).getUserById(any());
+        verifyNoMoreInteractions(domainService);
+    }
+
+    @Test
+    void editRolesTest() {
+        when(domainService.editRolesUser(any())).thenReturn(getUserResponse().getUserId());
+        when(domainService.getUserById(any())).thenReturn(getUserResponse());
+
+        var result = service.editRolesUser(getUserEditRolesRequest());
+
+        assertEquals(ID, result.getUserId());
+        assertEquals(NAME,result.getFullName());
+        assertEquals(LOGIN, result.getLogin());
+        assertTrue(result.getRoles().isEmpty());
+        assertEquals(CREATED,result.getCreated());
+        assertEquals(MODIFIED, result.getModified());
+
+        verify(domainService).editUser(any());
+        verify(domainService).getUserById(any());
+        verifyNoMoreInteractions(domainService);
+    }
+
+    @Test
+    void editPasswordUserTest(){
+        when(domainService.editPasswordUser(any())).thenReturn(getUserResponse().getUserId());
+        when(domainService.getUserById(any())).thenReturn(getUserResponse());
+
+        var result = service.editPasswordUser(getUserEditPasswordRequest("QwEr123145#"));
+
+
+        assertEquals(ID, result.getUserId());
+        assertEquals(NAME,result.getFullName());
+        assertEquals(LOGIN, result.getLogin());
+        assertTrue(result.getRoles().isEmpty());
+        assertEquals(CREATED,result.getCreated());
+        assertEquals(MODIFIED, result.getModified());
+
+        verify(domainService).editUser(any());
+        verify(domainService).getUserById(any());
+        verifyNoMoreInteractions(domainService);
+
+    }
+
     private UserResponse getUserResponse() {
         return new UserResponse()
                 .setUserId(ID)
@@ -82,12 +188,29 @@ import static org.mockito.Mockito.when;
 
     }
 
+    private UserRequest getUserRequest() {
+        return new UserRequest()
+                .setUserId(ID);
+    }
+
     private UserAddRequest getUserAddRequest(String pwd) {
         return new UserAddRequest()
                 .setId(ID)
                 .setName(NAME)
                 .setLogin(LOGIN)
                 .setPassword(pwd);
+    }
+
+    private UserEditPasswordRequest getUserEditPasswordRequest(String pwd){
+        return new UserEditPasswordRequest()
+                .setId(ID)
+                .setPassword(pwd);
+    }
+
+    private UserEditRolesRequest getUserEditRolesRequest(){
+        return new UserEditRolesRequest()
+                .setId(ID)
+                .setRoles(ROLES);
     }
 
 }
